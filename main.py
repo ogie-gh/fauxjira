01907d314312e1b5a6143de3048a0858f2695ce5
ogie-gh <144860767+ogie-gh@users.noreply.github.com>
2023-10-24 15:24:09 +1030

    Update main.py

--- main.py
+++ main.py
@@ -32,46 +32,55 @@ def load_template(template_name):
     with open(template_path, 'r') as template_file:
         return json.load(template_file)
 
-def create_jira_json(team, json_input):
+def create_jira_json(json_input):
     try:
         config_data = load_config()
         if config_data is not None:
-            mapping_data = get_mapping(config_data, team)
-            if mapping_data is not None:
-                config_key = mapping_data['key']
-                config_labels = mapping_data.get('labels', [])
+            input_data = json.loads(json_input)
 
-                # Load the provided JSON input
-                input_data = json.loads(json_input)
+            # Initialize a list to store the generated Jira JSON for each input item
+            jira_json_list = []
 
-                # Check if a custom template is requested
-                if input_data.get('template') == 'custom':
-                    template_data = input_data
-                else:
-                    # Load the template based on the template name
-                    template_data = load_template(input_data['template'])
+            for item in input_data:
+                team = item.get('team')
+                if not team:
+                    print("Team value is not provided in one of the input items.")
+                    continue
 
-                # Populate the template with variables
-                template_data['fields']['project']['key'] = config_key
-                template_data['labels'] = config_labels
+                mapping_data = get_mapping(config_data, team)
+                if mapping_data is not None:
+                    config_key = mapping_data['key']
+                    config_labels = mapping_data.get('labels', [])
 
-                # Open the CSV file and add its contents to the JSON
-                if 'filename' in input_data:
-                    with open(input_data['filename'], 'r') as csv_file:
-                        csv_contents = csv_file.read()
-                        # Replace the placeholder in the template with CSV contents
-                        template_data['fields']['description']['content'][1]['content'][0]['text'] = csv_contents
+                    # Check if a custom template is requested
+                    if item.get('template') == 'custom':
+                        template_data = item
+                    else:
+                        # Load the template based on the template name
+                        template_data = load_template(item['template'])
 
-                return json.dumps(template_data, indent=4)
+                    # Populate the template with variables
+                    template_data['fields']['project']['key'] = config_key
+                    template_data['labels'] = config_labels
+
+                    # Open the CSV file and add its contents to the JSON
+                    if 'filename' in item:
+                        with open(item['filename'], 'r') as csv_file:
+                            csv_contents = csv_file.read()
+                            # Replace the placeholder in the template with CSV contents
+                            template_data['fields']['description']['content'][1]['content'][0]['text'] = csv_contents
+
+                    jira_json_list.append(template_data)
+
+            return json.dumps(jira_json_list, indent=4)
     except Exception as e:
         print(f"An error occurred: {str(e)}")
         return None
 
 @click.command()
-@click.option('--team', help="The team you want to raise a Jira to", required=True)
-@click.option('--json-input', type=click.STRING)
-def main(team, json_input):
-    jira_json = create_jira_json(team, json_input)
+@click.option('--json-input', type=click.STRING, required=True)
+def main(json_input):
+    jira_json = create_jira_json(json_input)
     if jira_json:
         print(jira_json)
 
